a934e8f48ed9b028779af1a501681ccb72d0d71a
queenie-z <47308198+queenie-z@users.noreply.github.com>
2019-08-28 17:28:22 -0600

    Create Assignment 2 - Matrix.h
    Task: Write a Matrix class, which represents an m x n matrix of integers (Matrix.h and Matrix.cc).

---
+++ Assignment 2 - Matrix.h
@@ -0,0 +1,40 @@
+//CPSC 2620
+//File: Matrix.h
+//This is a header file for Matrix class.
+//The matrix is defined by m x n.
+//Author: Queenie Zhao
+//Date: Jan 31, 2019
+
+#ifndef MATRIX_H
+#define MATRIX_J
+#include <iostream>
+
+using namespace std;
+
+class Matrix
+{
+  public:
+   
+   Matrix(int m = 2, int n = 2); //Default constructor. m = n = 2, all values are set to zero.
+
+   void getDimensions(int& m, int& n); //Returns the dimensions of the matrix.
+
+   int& element(int i, int j);
+   const int& constElement(int i, int j) const;
+
+   void add(const Matrix& M); //Adds two matrices of the same parameters together.
+   void subtract(const Matrix& M); //Subtracts a matrix from another, both with the same parameters.
+   Matrix multiply(const Matrix& M)const; //Multiplies two matrices together.
+
+   void read(); //Reads in matrix numbers with cin
+   void write() const; //Prints out matrix numbers with cout
+   
+
+  private:
+   int rows;
+   int cols;
+   int **A;
+
+};
+
+#endif
