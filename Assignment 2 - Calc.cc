a94da5f2d04fedef9a9b2b736e9aef9c526704e0
queenie-z <47308198+queenie-z@users.noreply.github.com>
2019-08-28 17:31:50 -0600

    Create Assignment 2 - Calc.cc
    Task: Using the Matrix class, write a program (calc.cc) that:
                (a) Reads in three matrices A, B, and C.
                (b) Sets D to be the same matrix as A except that entries in the first column are 0.
                (c) Computes and displays the following results:
                    - A + B
                    - A x C - A
                    - (A + D) x C

---
+++ Assignment 2 - Calc.cc
@@ -0,0 +1,108 @@
+//CPSC 2620
+//File: Matrix.h
+//This is a test file for the Matrix class to perform addition, subtraction, and multiplication.
+//Author: Queenie Zhao
+//Date: Jan 31, 2019
+
+#include "./Matrix.h"
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+   Matrix A, B, C;
+   int r1, c1;
+
+   //Reading in matrices A, B, and C.
+   cout << "Now executing A.read():" << endl;
+   A.read();
+   cout << "Now executing B.read():" << endl;
+   B.read();
+   cout << "Now executing C.read():" << endl;
+   C.read();
+   
+   //Creating matrix D and initializes all elements of A to D, then all entries of the first column to zero.
+   A.getDimensions(r1, c1);
+   Matrix D(r1, c1);
+
+   for(int i = 0; i < r1; i++)
+  	for(int j = 0; j < c1; j++)
+     D.element(i, j) = A.element(i, j);
+   for(int i = 0; i < r1; i++)
+  	D.element(i, 0) = 0;
+
+   //Printing matrices A, B, C, and D.
+   cout << "A is:" << endl;
+   A.write();
+   cout << endl;
+   cout << "B is:" << endl;
+   B.write();
+   cout << endl;
+   cout << "C is:" << endl;
+   C.write();
+   cout << endl;
+   cout << "D is:" << endl;
+   D.write();
+   cout << endl;
+
+   //Executing and printing A + B (without operator overloading).
+   cout << "After A.add(B), A is now:" << endl;
+   A.add(B);
+   A.write();
+   cout << endl;
+   //Printing matrix B to let user know it has not changed.
+   cout << "And B is still:" << endl;
+   B.write();
+   cout << endl;
+
+   //Creating matrix E and initialize dimensions to row of A and column of C.
+   int r2, r3, c2, c3;
+   A.getDimensions(r2, c2);
+   C.getDimensions(r3, c3);
+   Matrix E(r2, c3);
+
+   //Executing and printing A * C - A (without operator overloading).
+   cout << "After E = A.multiply(C) and E.subtract(A), the new product E is: " << endl;
+   E = A.multiply(C);
+   E.subtract(A);
+   E.write();
+   cout << endl;
+
+   //Printing matrices used, A, C, and D, to let use know they have not changed.
+   cout << "A is still:" << endl;
+   A.write();
+   cout << endl;
+   cout << "C is still:" << endl;
+   C.write();
+   cout << endl;
+   cout << "D is still:" << endl;
+   D.write();
+   cout << endl;
+   
+   //Creating matrix F and initialize dimensions to row of A and column of C.
+   int r4, r5, c4, c5;
+   A.getDimensions(r4, c4);
+   C.getDimensions(r5, c5);
+   Matrix F(r4, c5);
+
+   //Executing and printing (A + D) * C (without operator overloading).
+   cout << "After A.add(D) and F = A.multiply(C), the new product F is: " << endl;
+   A.add(D);
+   F = A.multiply(C);
+   F.write();
+   cout << endl;
+   
+   //Printing matrices used. A have changed while C and D remains the same.
+   cout << "A is now:" << endl;
+   A.write();
+   cout << endl;
+   cout << "C is still:" << endl;
+   C.write();
+   cout << endl;
+   cout << "D is still:" << endl;
+   D.write();
+   cout << endl;
+
+   return 0;
+}
