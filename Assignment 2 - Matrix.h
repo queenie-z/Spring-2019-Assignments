a0e9b2cb4e9356cd9228fa32d7b3ddea9652afce
queenie-z <47308198+queenie-z@users.noreply.github.com>
2019-08-28 22:41:19 -0600

    Update Assignment 2 - Matrix.h

--- Assignment 2 - Matrix.h
+++ Assignment 2 - Matrix.h
@@ -13,28 +13,27 @@ using namespace std;
 
 class Matrix
 {
+  private:
+   int rows;
+   int cols;
+   int **A;
+  
   public:
    
    Matrix(int m = 2, int n = 2); //Default constructor. m = n = 2, all values are set to zero.
 
    void getMxNDimen(int& m, int& n);
 
-   int& element(int i, int j);
-   const int& constElement(int i, int j) const;
+   int& returnElemAt(int i, int j);
+   const int& constElemAt(int i, int j) const;
 
-   void add(const Matrix& M); //Adds two matrices of the same parameters together.
-   void subtract(const Matrix& M); //Subtracts a matrix from another, both with the same parameters.
-   Matrix multiply(const Matrix& M)const; //Multiplies two matrices together.
-
-   void read(); //Reads in matrix numbers with cin
-   void write() const; //Prints out matrix numbers with cout
+   void addMatrixOfSameDimen(const Matrix& M);
+   void subtractMatrixOfSameDimen(const Matrix& M);
+   Matrix multiplyMatrices(const Matrix& M)const;
+  
+   void assembleMatrix(); 
+   void printMatrix() const; //Prints out matrix numbers with cout
    
-
-  private:
-   int rows;
-   int cols;
-   int **A;
-
 };
 
 #endif
